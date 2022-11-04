de27859f58763bb6cca55ebd17aabb4157b36091
Steve Lau <stevelauc@outlook.com>
2022-11-04 10:35:09 +0800

    test: add test for macOS bindings

--- test/test.rs
+++ test/test.rs
@@ -3,9 +3,8 @@
 mod test_linux_android_netbsd {
     use errno::Errno;
     use extattr::{
-        fgetxattr, flistxattr, fremovexattr, fsetxattr, getxattr, lgetxattr,
-        listxattr, llistxattr, lremovexattr, lsetxattr, removexattr, setxattr,
-        Result, SetxattrFlag,
+        fgetxattr, flistxattr, fremovexattr, fsetxattr, getxattr, listxattr,
+        removexattr, setxattr, SetxattrFlag,
     };
     use std::{fs::File, os::unix::io::AsRawFd};
 
@@ -44,6 +43,76 @@ mod test_linux_android_netbsd {
         );
 
         assert_eq!(res, Err(Errno(libc::ENOENT)));
+    }
+
+    /// Test flag `XATTR_CREATE`
+    #[test]
+    fn test_setxattr_xattr_create() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path = temp_dir.path().join("test_setxattr_xattr_create");
+        File::create(temp_file_path.as_path()).unwrap();
+
+        // set it first
+        let res_set = setxattr(
+            temp_file_path.as_path(),
+            "user.test_setxattr_xattr_create",
+            "",
+            SetxattrFlag::empty(),
+        );
+
+        // EA is not supported on the underlying file system, skip the test.
+        if let Err(Errno(libc::ENOTSUP)) = res_set {
+            return;
+        }
+
+        // Then try to set it again, with `XATTR_CREATE` flag
+        let res = setxattr(
+            temp_file_path.as_path(),
+            "user.test_setxattr_xattr_create",
+            "",
+            SetxattrFlag::XATTR_CREATE,
+        );
+
+        assert_eq!(res, Err(Errno(libc::EEXIST)));
+    }
+
+    /// Test flag `XATTR_REPLACE`
+    #[test]
+    fn test_setxattr_xattr_replace() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path =
+            temp_dir.path().join("test_setxattr_xattr_replace");
+        File::create(temp_file_path.as_path()).unwrap();
+
+        let res = setxattr(
+            temp_file_path.as_path(),
+            "user.test_setxattr_xattr_replace",
+            "",
+            SetxattrFlag::XATTR_REPLACE,
+        );
+
+        match res {
+            // The underlying file system does not support EA, skip this test.
+            Err(Errno(libc::ENOTSUP)) => {}
+            _ => assert_eq!(res, Err(Errno(libc::ENODATA))),
+        }
+    }
+
+    #[test]
+    fn test_setxattr_invalid_namespace() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path =
+            temp_dir.path().join("test_setxattr_invalid_namespace");
+        File::create(temp_file_path.as_path()).unwrap();
+
+        let res = setxattr(
+            temp_file_path.as_path(),
+            "xxxx.test_setxattr_invalid_namespace",
+            "",
+            SetxattrFlag::empty(),
+        );
+
+        assert_eq!(res, Err(Errno(libc::ENOTSUP)));
     }
 
     #[test]
@@ -129,6 +198,26 @@ mod test_linux_android_netbsd {
     }
 
     #[test]
+    fn test_getxattr_attribute_does_not_exist() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path = temp_dir
+            .path()
+            .join("test_getxattr_attribute_does_not_exist");
+        File::create(temp_file_path.as_path()).unwrap();
+
+        let res = getxattr(
+            temp_file_path.as_path(),
+            "user.test_getxattr_attribute_does_not_exist",
+        );
+
+        match res {
+            // EA is not supported, skip the test.
+            Err(Errno(libc::ENOTSUP)) => {}
+            _ => assert_eq!(res, Err(Errno(libc::ENODATA))),
+        }
+    }
+
+    #[test]
     fn test_fgetxattr() {
         let temp_dir = tempfile::tempdir_in("./").unwrap();
         let temp_file_path = temp_dir.path().join("test_fgetxattr");
@@ -192,20 +281,17 @@ mod test_linux_android_netbsd {
         File::create(temp_file_path.as_path()).unwrap();
 
         // Here, we use `setxattr(path, "user.*", value, flags)` instead of `listxattr`
-        // to test if EA is supported.
-        //
-        // This is necessary as we need to know whether `user` namespace EA is supported
-        // rather than other categories of EA.
-        //
-        // For example, on `tmpfs`, `trusted` and `security` namespace EAs are
-        // supported, but `user` is not.
+        // to test if EA is supported because on some file system (e.g., tmpfs), `user`
+        // EA is not supported but `trusted` and `security` EA are. Since we test
+        // `removexattr` using `user` EA, we need to know if `user` EA is supported on
+        // the underlying file system.
         if let Err(Errno(libc::ENOTSUP)) = setxattr(
             temp_file_path.as_path(),
             "user.ea",
             "",
             SetxattrFlag::empty(),
         ) {
-            // The underlying file system does not support EA, skip this test.
+            // The underlying file system does not support user EA, skip this test.
             return;
         }
 
@@ -318,9 +404,9 @@ mod test_freebsd_netbsd {
     }
 
     #[test]
-    fn test_flistxattr() {
+    fn test_extattr_list_fd() {
         let temp_dir = tempfile::tempdir_in("./").unwrap();
-        let temp_file_path = temp_dir.path().join("test_flistxattr");
+        let temp_file_path = temp_dir.path().join("test_extattr_list_fd");
         let temp_file = File::create(temp_file_path.as_path()).unwrap();
         let temp_file_fd = temp_file.as_raw_fd();
 
@@ -347,7 +433,7 @@ mod test_freebsd_netbsd {
             extattr_get_file(
                 temp_file_path.as_path(),
                 AttrNamespace::EXTATTR_NAMESPACE_USER,
-                "test_extattr_get_file"
+                "test_extattr_get_file",
             )
         );
     }
@@ -372,25 +458,25 @@ mod test_freebsd_netbsd {
             extattr_get_fd(
                 temp_file_fd,
                 AttrNamespace::EXTATTR_NAMESPACE_USER,
-                "test_extattr_get_fd"
+                "test_extattr_get_fd",
             )
         );
     }
 
-    /// call `extattr_get_file()` on a file that does not exist, return `ENOENT`
     #[test]
-    fn test_extattr_get_file_file_not_exist() {
+    fn test_extattr_get_file_ea_not_exist() {
         let temp_dir = tempfile::tempdir_in("./").unwrap();
         let temp_file_path =
-            temp_dir.path().join("test_extattr_get_file_file_not_exist");
+            temp_dir.path().join("test_extattr_get_file_ea_not_exist");
+        File::create(temp_file_path.as_path()).unwrap();
 
         assert_eq!(
             extattr_get_file(
                 temp_file_path,
                 AttrNamespace::EXTATTR_NAMESPACE_USER,
-                "test_extattr_get_file_file_not_exist",
+                "test_extattr_get_file_ea_not_exist",
             ),
-            Err(Errno(libc::ENOENT))
+            Err(Errno(libc::ENOATTR))
         );
     }
 
@@ -453,7 +539,7 @@ mod test_freebsd_netbsd {
         assert!(extattr_delete_fd(
             temp_file_fd,
             AttrNamespace::EXTATTR_NAMESPACE_USER,
-            "test_extattr_delete_fd"
+            "test_extattr_delete_fd",
         )
         .is_ok());
     }
@@ -461,4 +547,340 @@ mod test_freebsd_netbsd {
 
 #[cfg(test)]
 #[cfg(any(target_os = "macos", target_os = "ios"))]
-mod test_darwin {}
+mod test_darwin {
+    use errno::Errno;
+    use extattr::{
+        fgetxattr, flistxattr, fremovexattr, fsetxattr, getxattr, listxattr,
+        removexattr, setxattr, Options,
+    };
+    use std::{fs::File, os::unix::io::AsRawFd};
+
+    #[test]
+    fn test_setxattr_file_exist() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path = temp_dir.path().join("test_setxattr_file_exist");
+        File::create(temp_file_path.as_path()).unwrap();
+
+        let res = setxattr(
+            temp_file_path.as_path(),
+            "user.test_setxattr_file_exist",
+            "",
+            0,
+            Options::empty(),
+        );
+
+        match res {
+            // The underlying file system does not support EA, skip this test.
+            Err(Errno(libc::ENOTSUP)) => {}
+            // If EA is supported, then no error should occur
+            _ => assert!(res.is_ok()),
+        }
+    }
+
+    #[test]
+    fn test_setxattr_file_not_exist() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path =
+            temp_dir.path().join("test_setxattr_file_not_exist");
+
+        let res = setxattr(
+            temp_file_path.as_path(),
+            "user.test_setxattr_file_not_exist",
+            "",
+            0,
+            Options::empty(),
+        );
+
+        assert_eq!(res, Err(Errno(libc::ENOENT)));
+    }
+
+    /// Test flag `XATTR_CREATE`
+    #[test]
+    fn test_setxattr_xattr_create() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path = temp_dir.path().join("test_setxattr_xattr_create");
+        File::create(temp_file_path.as_path()).unwrap();
+
+        // set it first
+        let res_set = setxattr(
+            temp_file_path.as_path(),
+            "user.test_setxattr_xattr_create",
+            "",
+            0,
+            Options::empty(),
+        );
+
+        // EA is not supported on the underlying file system, skip the test.
+        if let Err(Errno(libc::ENOTSUP)) = res_set {
+            return;
+        }
+
+        // Then try to set it again, with `XATTR_CREATE` flag
+        let res = setxattr(
+            temp_file_path.as_path(),
+            "user.test_setxattr_xattr_create",
+            "",
+            0,
+            Options::XATTR_CREATE,
+        );
+
+        assert_eq!(res, Err(Errno(libc::EEXIST)));
+    }
+
+    /// Test flag `XATTR_REPLACE`
+    #[test]
+    fn test_setxattr_xattr_replace() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path =
+            temp_dir.path().join("test_setxattr_xattr_replace");
+        File::create(temp_file_path.as_path()).unwrap();
+
+        let res = setxattr(
+            temp_file_path.as_path(),
+            "user.test_setxattr_xattr_replace",
+            "",
+            0,
+            Options::XATTR_REPLACE,
+        );
+
+        match res {
+            // The underlying file system does not support EA, skip this test.
+            Err(Errno(libc::ENOTSUP)) => {}
+            _ => assert_eq!(res, Err(Errno(libc::ENOATTR))),
+        }
+    }
+
+    #[test]
+    fn test_fsetxattr() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path = temp_dir.path().join("test_fsetxattr");
+        let temp_file = File::create(temp_file_path.as_path()).unwrap();
+        let temp_file_fd = temp_file.as_raw_fd();
+
+        let res = fsetxattr(
+            temp_file_fd,
+            "user.test_fsetxattr",
+            "",
+            0,
+            Options::empty(),
+        );
+
+        match res {
+            // The underlying file system does not support EA, skip this test.
+            Err(Errno(libc::ENOTSUP)) => {}
+            // If EA is supported, then no error should occur
+            _ => assert!(res.is_ok()),
+        }
+    }
+
+    #[test]
+    fn test_listxattr() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path = temp_dir.path().join("test_listxattr");
+        File::create(temp_file_path.as_path()).unwrap();
+
+        let res = listxattr(temp_file_path.as_path(), Options::empty());
+
+        match res {
+            // The underlying file system does not support EA, skip this test.
+            Err(Errno(libc::ENOTSUP)) => {}
+            // If EA is supported, then no error should occur
+            _ => assert!(res.is_ok()),
+        }
+    }
+
+    #[test]
+    fn test_flistxattr() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path = temp_dir.path().join("test_flistxattr");
+        let temp_file = File::create(temp_file_path.as_path()).unwrap();
+        let temp_file_fd = temp_file.as_raw_fd();
+
+        let res = flistxattr(temp_file_fd, Options::empty());
+
+        match res {
+            // The underlying file system does not support EA, skip this test.
+            Err(Errno(libc::ENOTSUP)) => {}
+            // If EA is supported, then no error should occur
+            _ => assert!(res.is_ok()),
+        }
+    }
+
+    #[test]
+    fn test_getxattr() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path = temp_dir.path().join("test_getxattr");
+        File::create(temp_file_path.as_path()).unwrap();
+
+        let res = setxattr(
+            temp_file_path.as_path(),
+            "user.test_getxattr",
+            "",
+            0,
+            Options::empty(),
+        );
+
+        // The underlying file system does not support EA, skip this test.
+        if let Err(Errno(libc::ENOTSUP)) = res {
+            return;
+        }
+
+        // If EA is supported, then no error should occur
+        assert!(res.is_ok());
+
+        assert_eq!(
+            Ok(Vec::new()),
+            getxattr(
+                temp_file_path.as_path(),
+                "user.test_getxattr",
+                0,
+                Options::empty()
+            )
+        );
+    }
+
+    #[test]
+    fn test_getxattr_attribute_does_not_exist() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path = temp_dir
+            .path()
+            .join("test_getxattr_attribute_does_not_exist");
+        File::create(temp_file_path.as_path()).unwrap();
+
+        let res = getxattr(
+            temp_file_path.as_path(),
+            "user.test_getxattr_attribute_does_not_exist",
+            0,
+            Options::empty(),
+        );
+
+        match res {
+            // EA is not supported, skip the test.
+            Err(Errno(libc::ENOTSUP)) => {}
+            _ => assert_eq!(res, Err(Errno(libc::ENOATTR))),
+        }
+    }
+
+    #[test]
+    fn test_fgetxattr() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path = temp_dir.path().join("test_fgetxattr");
+        let temp_file = File::create(temp_file_path).unwrap();
+        let temp_file_fd = temp_file.as_raw_fd();
+
+        let res = fsetxattr(
+            temp_file_fd,
+            "user.test_fgetxattr",
+            "",
+            0,
+            Options::empty(),
+        );
+
+        // The underlying file system does not support EA, skip this test.
+        if let Err(Errno(libc::ENOTSUP)) = res {
+            return;
+        }
+
+        // If EA is supported, then no error should occur
+        assert!(res.is_ok());
+
+        assert_eq!(
+            Ok(Vec::new()),
+            fgetxattr(temp_file_fd, "user.test_fgetxattr", 0, Options::empty())
+        );
+    }
+
+    #[test]
+    fn test_removexattr_ea_exist() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path = temp_dir.path().join("test_removexattr_ea_exist");
+        File::create(temp_file_path.as_path()).unwrap();
+
+        let res = setxattr(
+            temp_file_path.as_path(),
+            "user.test_removexattr_ea_exist",
+            "",
+            0,
+            Options::empty(),
+        );
+
+        // The underlying file system does not support EA, skip this test.
+        if let Err(Errno(libc::ENOTSUP)) = res {
+            return;
+        }
+
+        // If EA is supported, then no error should occur
+        assert!(res.is_ok());
+
+        assert!(removexattr(
+            temp_file_path.as_path(),
+            "user.test_removexattr_ea_exist",
+            Options::empty(),
+        )
+        .is_ok());
+    }
+
+    #[test]
+    fn test_removexattr_ea_not_exist() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path =
+            temp_dir.path().join("test_removexattr_ea_not_exist");
+        File::create(temp_file_path.as_path()).unwrap();
+
+        // Here, we use `setxattr(path, "user.*", value, flags)` instead of `listxattr`
+        // to test if EA is supported because on some file system (e.g., tmpfs), `user`
+        // EA is not supported but `trusted` and `security` EA are. Since we test
+        // `removexattr` using `user` EA, we need to know if `user` EA is supported on
+        // the underlying file system.
+        if let Err(Errno(libc::ENOTSUP)) = setxattr(
+            temp_file_path.as_path(),
+            "user.ea",
+            "",
+            0,
+            Options::empty(),
+        ) {
+            // The underlying file system does not support user EA, skip this test.
+            return;
+        }
+
+        assert_eq!(
+            Err(Errno(libc::ENOATTR)),
+            removexattr(
+                temp_file_path.as_path(),
+                "user.test_removexattr_ea_not_exist",
+                Options::empty(),
+            )
+        );
+    }
+
+    #[test]
+    fn test_fremovexattr() {
+        let temp_dir = tempfile::tempdir_in("./").unwrap();
+        let temp_file_path = temp_dir.path().join("test_fremovexattr");
+        let temp_file = File::create(temp_file_path.as_path()).unwrap();
+        let temp_file_fd = temp_file.as_raw_fd();
+
+        let res = fsetxattr(
+            temp_file_fd,
+            "user.test_fremovexattr",
+            "",
+            0,
+            Options::empty(),
+        );
+
+        // The underlying file system does not support EA, skip this test.
+        if let Err(Errno(libc::ENOTSUP)) = res {
+            return;
+        }
+
+        // If EA is supported, then no error should occur
+        assert!(res.is_ok());
+
+        assert!(fremovexattr(
+            temp_file_fd,
+            "user.test_fremovexattr",
+            Options::empty()
+        )
+        .is_ok());
+    }
+}
